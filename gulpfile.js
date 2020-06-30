83f4970f439caa294ede3d422d37a5b1378a9bd4
vovafish <vladimirribakov123@mail.ru>
2020-06-30 20:42:55 +0300

    feat: added gulp-sass into gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-const cssnano = require('gulp-cssnano');
+const sass = require('gulp-sass');
+// const cssnano = require('gulp-cssnano');
 
-
-gulp.task('hello', function (done) {
-  console.log('Hello World');
-  done();
-});
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
