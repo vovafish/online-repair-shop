b4070fee286996a44cd521f23732634a99f8e745
vovafish <vladimirribakov123@mail.ru>
2020-07-02 17:47:23 +0300

    refact/rewrite: added lines to watch scss files aswell in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,13 @@ function bs() {
     });
     watch("./*.html").on('change', browserSync.reload);
     watch("./sass/**/*.sass", serveSass);
+    watch("./sass/**/*.scss", serveSass);
     watch("./js/*.js").on('change', browserSync.reload);
 };
 
 // Compile sass into CSS & auto-inject into browsers
 function serveSass() {
-    return src("./sass/*.sass")
+    return src("./sass/**/*.sass", "./sass/*.scss")
         .pipe(sass())
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
