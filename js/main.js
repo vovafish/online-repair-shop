85ab44212092645807e581b7f2f9e6f60932eff4
vovafish <vladimirribakov123@mail.ru>
2020-07-08 18:15:39 +0300

    refact/feat: changed js ES6 code for modal window to jQuery version. Added scroll up button.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,50 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
+/* Modal window but on ES6 */
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close');
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible');
+//   }
   
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click', switchModal);
+//   });
+
+//   closeBtn.addEventListener('click', switchModal);
+
+// });
+
+/* Modal window */
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close');
+    
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
   });
-
-  closeBtn.addEventListener('click', switchModal);
-
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
 });
+
+/* Button to scroll up */
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
