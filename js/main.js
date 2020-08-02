fdb281dbe9168ac08ae4669933050b00a36800ab
vovafish <vladimirribakov123@mail.ru>
2020-08-02 17:12:05 +0300

    refact/feat: Fixed video / Did whole desktop version

--- js/main.js
+++ js/main.js
@@ -373,21 +373,23 @@ ymaps.ready(function () {
 
   /* IFrame Player API */
 
-  var player;
-  $('.video__play').on('click', function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      height: '465',
-      width: '100%',
-      videoId: '54bAimQUGi4',
-      events: {
-        'onReady': videoPlay,
 
-      }
-    });
-  })
-
-  function videoPlay(event) {
-    event.target.videoPlay();
-  }
 
 });
+
+var player;
+$('.video__play').on('click', function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '460',
+    width: '100%',
+    videoId: '54bAimQUGi4',
+    events: {
+      'onReady': videoPlay,
+
+    }
+  });
+})
+
+function videoPlay(event) {
+  event.target.playVideo();
+}
